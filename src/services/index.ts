9d317d019c4f919905d217ec61d2f2d86102cb0c
tan <jolanutan@gmail.com>
2024-06-26 22:12:36 +0800

    refactor: pages changes on migration (#9)

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,4 +1,6 @@
 import { prisma } from '../../db'
 import { TransactionService } from './transaction'
+import { CustomerService } from './customer'
 
 export const transaction = new TransactionService(prisma)
+export const customer = new CustomerService(prisma)
