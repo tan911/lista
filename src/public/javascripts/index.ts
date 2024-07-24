4885db158a3c4fbaa3586be615a0a7b9b87eae1d
tan <jolanutan@gmail.com>
2024-07-24 19:28:39 +0800

    feat: add user profile dropdown

--- src/public/javascripts/index.ts
+++ src/public/javascripts/index.ts
@@ -1,9 +1,10 @@
-import { CreditModal, NavigationBar, Auth } from './controllers'
+import { CreditModal, NavigationBar, Auth, Dropdown } from './controllers'
 
 function init() {
     new CreditModal('add-credit-modal')
     new NavigationBar('navbar')
     new Auth('auth-page')
+    new Dropdown('user-profile')
 }
 
 init()
